bf79a8cad7f575a1de8357c858646e0f96db62a3
Gabriel <gabrieldouradogarcez@gmail.com>
2022-08-09 19:32:43 -0300

    Iniciando projeto

---
+++ src/main/java/com/tcc/psiauth/PsiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.psiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/psiauth/PsiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.psiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
