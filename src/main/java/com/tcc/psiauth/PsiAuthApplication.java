93cf6fd51c154b2599af0ed5cd055e54eecb1faa
Gabriel <gabrieldouradogarcez@gmail.com>
2022-08-09 19:37:56 -0300

    Tornando um eureka client

--- src/main/java/com/tcc/psiauth/PsiAuthApplication.java
+++ src/main/java/com/tcc/psiauth/PsiAuthApplication.java
@@ -2,8 +2,10 @@ package com.tcc.psiauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PsiAuthApplication {
 
 	public static void main(String[] args) {
